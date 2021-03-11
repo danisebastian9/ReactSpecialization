953721f6d4630bd5e5b0d308c21499a37c72964c
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-03-11 15:43:01 -0500

    loading state added

--- Class2/giffy/src/components/ListOfGifs.js
+++ Class2/giffy/src/components/ListOfGifs.js
@@ -5,13 +5,20 @@ import getGifs from "../services/getGifs"
 export default function ListOfGifs ({ params }) {
 
     const { keyword } = params
+    const [loading, setLoading] = useState(false)
 
     const [gifs, setGifs] = useState([])
 
     useEffect(function () {
+        setLoading(true)
       getGifs({ keyword })
-      .then(gifs => setGifs(gifs))
+      .then(gifs => {
+        setGifs(gifs)
+        setLoading(false)
+      })
     }, [keyword] )
+
+    if (loading) return <i>Loading...  emojis</i>
 
     return <div>
         {
