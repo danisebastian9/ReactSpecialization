a32ec3e8641f41594b53953efddd050586831801
danisebastian9 <44168842+danisebastian9@users.noreply.github.com>
2021-03-11 23:11:24 -0500

    Pages updated

--- Class2/giffy/src/pages/Home/index.js
+++ Class2/giffy/src/pages/Home/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
-import { Link } from "wouter"
+import React, {useState} from 'react'
+import { Link, useLocation } from "wouter"
+
 
 const POPULAR_GIFS = ["Pandas", "Development", "Colombia", "Movies", "Technology"]
 
 export default function Home() {
+    const [keyword, setKeyword] = useState("")
+    const [path, pushLocation] = useLocation()
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        pushLocation(`/search/${keyword}`)
+    }
+
+    const handleChange = event => {
+        setKeyword(event.target.value)
+    }
+
     return (
         <>
+            <form onSubmit={handleSubmit}>
+                <input placeholder="Search a Gif here..." onChange={handleChange} type="text" value={keyword} />
+            </form>
             <h3 className="App-title">Los Gifs más populares</h3>
             <ul>
                 {POPULAR_GIFS.map((popularGif) => (
--- Class2/giffy/src/pages/SearchResults/index.js
+++ Class2/giffy/src/pages/SearchResults/index.js
@@ -1,5 +1,26 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
+import Spinner from "../../components/Spinner"
+import ListOfGifs from "../../components/ListOfGifs"
+import getGifs from "../../components/getGifs"
 
-export default function SearchResult() {
-    return(console.log(SearchResult))
+export default function SearchResult({ params }) {
+    const { keyword } = params
+    const [loading, setLoading] = useState(false)
+    const [gifs, setGifs] = useState([])
+
+    useEffect(function () {
+        setLoading(true)
+        getGifs({ keyword })
+            .then(gifs => {
+                setGifs(gifs)
+                setLoading(false)
+            })
+    }, [keyword])
+
+    return <>
+        {loading
+            ? <Spinner />
+            : <ListOfGifs gifs={gifs} />
+        }
+    </>
 }
